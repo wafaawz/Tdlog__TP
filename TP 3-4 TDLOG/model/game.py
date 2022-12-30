92662881561c2f1fef66e6cadd46b6c22e25bdc9
wafaawz <114984584+wafaawz@users.noreply.github.com>
2022-12-30 17:07:33 +0100

    Add files via upload

---
+++ TP 3-4 TDLOG/model/game.py
@@ -0,0 +1,20 @@
+from model.exceptions import GameFullError
+from model.player  import Player
+
+
+class Game:
+    def __init__(self, id=None):
+        self.id = id
+        self.players = []
+
+    def get_id(self) -> int:
+        return self.id
+
+    def get_players(self) -> list[Player]:
+        return self.players
+
+    def add_player(self, player: Player):
+        if len(self.players) >= 2:
+            raise GameFullError(
+                "Seulement 2 joueurs sont admis dans la partie !")
+        self.players.append(player)
---
+++ TP 3-4 TDLOG/model/player.py
@@ -0,0 +1,14 @@
+from model.battlefield import Battlefield
+
+
+class Player:
+    def __init__(self, name: str, battlefield: Battlefield):
+        self.id = None
+        self.name = name
+        self.battlefield = battlefield
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_battlefield(self) -> Battlefield:
+        return self.battlefield
